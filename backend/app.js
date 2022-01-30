16e2a143fa474d58bc615951e9deb2b79db6849c
Ethan Lam <ethan.lam@mail.utoronto.ca>
2022-01-30 04:07:11 -0500

    push change in styles to make porkbun hosting work

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+app.post("/send-image", (req, res) => {
+    console.log(req.body);
+    res.send("ok");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
